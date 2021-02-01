f4784373e641e090e2d090cc69ed512966bdb0fb
Ivan Lanuza <icdlanuza@gmail.com>
2021-02-02 06:55:34 +0800

    complete register validations

--- src/components/pages/register.vue
+++ src/components/pages/register.vue
@@ -5,6 +5,7 @@
                 <v-col cols="0" md="2"></v-col>  
                 <v-col cols="12" md="8">
                     <v-card class=pa-4>
+                        <v-form ref="form">
                         <v-card-title>
                             Create an account.
                         </v-card-title>
@@ -33,9 +34,10 @@
                             placeholder="You'll use this to manage your account."
                             outlined
                             v-model="account_email"
+                            :error-messages="errors_email"
                             v-bind:hint=success_email                              
                             @input="checkEmail"                            
-                            :rules="[rules.required, rules.validEmail]"                    
+                            :rules="[rules.required]"                    
                         ></v-text-field>
                         <v-row>
                             <v-col cols="6">
@@ -67,11 +69,12 @@
                             <HR/><BR/>
                             <v-btn 
                                 block 
-                                color="primary"       
+                                color="primary"
+                                @click=submitForm       
                                 x-large>
                                     Create Account
                             </v-btn>
-                                  
+                        </v-form>          
                     </v-card>
                 </v-col>
                 <v-col cols="0" md="2"></v-col>  
@@ -90,54 +93,80 @@
                 account_url: '',
                 success_url: '',
                 errors_url: '',
+                account_email: '',
+                success_email: '',
+                errors_email: '',
                 show1: false,
                 show2: false,
                 confirm_password: '',
                 original_password: '',
-                account_email: '',
-                success_email: '',
+                val_url_unused: false,
+                val_url_chars: false,
+                val_email_unused: false,
+                val_email_chars: false,
                 rules: {
                     required: value => !!value || 'Required.',
                     length: value => value.length >= 5 || 'Minimum 5 characters',  
                     length8: value => value.length >= 8 || 'Minimum 8 characters',  
                     passwordMatch: () => ( this.original_password == this.confirm_password ) || (`The email and password you entered don't match`),
-                    validEmail: function(account_email) {
-                        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                        return re.test(account_email);                    
-                    } || ('Invalid Email')
                 }
             }
         },   
         methods: {
             checkURL() {
-                this.errors_url = '';  
-                this.success_url = '';  
                 if (this.account_url && this.account_url.length > 4) {
+                    this.errors_url = '';  
+                    this.success_url = '';  
                     simplebookService.checkURL(this.account_url)
                     .then(response => {
                         if (response.data.flag == true) {
                             this.success_url = "Very Nice! Your link will be: http://simplebook.it/" + this.account_url;
+                            this.val_url_unused = true;
                         } else {
-                            this.errors_url = "Sorry, URL already taken.";
+                            this.errors_url = "Sorry, this account URL is already in use.";
+                            this.val_url_unused = false;
                         }
                     })
                     .catch( () => {
                             this.errors_url = "Sorry! This Account URL is not valid.";
+                            this.val_url_unused = false;
                     });
+
+                    var alphatester = /^[A-Za-z0-9]+$/;
+                    if (alphatester.test(this.account_url) == false) {
+                        this.errors_url = "Please use letters or numbers only.";
+                        this.val_url_chars = false;
+                    } else {
+                        this.val_url_chars = true;
+                    }
                 }
             },
-            checkEmail() {          
-                simplebookService.checkEmail(this.account_email)
-                .then(response => {
-                    if (response.data.flag == false) {
-                        this.success_email = "E-mail already in use.";
+            checkEmail() {       
+                if(this.account_email) {   
+                    this.errors_email = '';
+                    this.success_email = '';
+                    simplebookService.checkEmail(this.account_email)
+                    .then(response => {
+                        if (response.data.flag == false) {
+                            this.errors_email = "This e-mail is already in use.";
+                            this.val_email_unused = false;
+                        } else {
+                            this.val_email_unused = true;
+                        }
+                    })
+                    .catch( () => {
+                            this.errors_email = "Sorry, unable to contact server.";
+                            this.val_email_unused = false;
+                        });
+
+                    var emailtester = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                    if (emailtester.test(this.account_email) == false) {
+                        this.errors_email = "Please use a valid email address.";
+                        this.val_email_chars = false;
                     } else {
-                        this.success_email = "";
+                        this.val_email_chars = true;
                     }
-                })
-                .catch( () => {
-                        this.success_email = "E-mail invalid.";
-                    });
+                }
             },   
             togglepass1() {
                     this.show1 = !this.show1;
@@ -147,6 +176,16 @@
             },             
             clear_pass2() {
                 this.confirm_password = "";
+            },
+            submitForm() {
+                if(this.$refs.form.validate() == true 
+                    && this.val_url_unused == true
+                    && this.val_url_chars == true
+                    && this.val_email_unused == true
+                    && this.val_email_chars == true
+                    ) {
+                    alert('all clear');
+                }
             }
         }
     };
